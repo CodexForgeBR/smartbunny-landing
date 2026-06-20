c2c77f2e73963e10c1b1f68bc897466ca64150c0
Forge <forge@codexforge.com.br>
2026-06-20 19:02:39 -0300

    Fix custom domain asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/smartbunny-landing/',
+  base: './',
   build: {
     target: 'es2020',
     assetsInlineLimit: 2048,
